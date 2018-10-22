fa9abb55999d1db1828e0611eb61a7aed17f3f15
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2018-10-22 18:44:31 +0900

    Create use-jquery-on-bookmarklet.js

---
+++ js/use-jquery-on-bookmarklet.js
@@ -0,0 +1,3 @@
+javascript:((f,s=document.createElement("script"))=>{s.src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js",s.onload=()=>f(jQuery.noConflict(!0));document.body.appendChild(s)})($=>{
+    console.log($().jquery);
+});
