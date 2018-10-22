de4039adc1537260ea592930bf733cceca9f8753
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2018-10-22 18:52:52 +0900

    Create screenshot.js

---
+++ js/rpgmv/screenshot.js
@@ -0,0 +1,11 @@
+(function(){
+	var now = (function(n){return(`${n.getFullYear()}${`0${n.getMonth()+1}`.slice(-2)}${`0${n.getDate()}`.slice(-2)}${`0${n.getHours()}`.slice(-2)}${`0${n.getMinutes()}`.slice(-2)}${`0${n.getSeconds()}`.slice(-2)}${`00${n.getMilliseconds()}`.slice(-3)}`)})(new Date());
+	var fn = `${document.URL.replace("file:///","").split("index.html")[0]}ss_${now}.png`;
+	require('nw.gui').Window.get().capturePage(function(buf){
+		require('fs').writeFile(fn,buf,function(e){
+			if(e) throw e;
+			var mes = `ScreenShot is saved!\n${fn}`
+			alert(mes); console.log(mes);
+		});
+	},{format:'png',datatype:'buffer'});
+})();
