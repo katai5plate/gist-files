ed2b21c680f3712697fe926a2c23f26eae6f2da6
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2018-10-23 11:01:17 +0900

    Create atsumaru-scraping.js

---
+++ js/atsumaru-scraping.js
@@ -0,0 +1,19 @@
+(()=>{
+    const b = document.body;
+    const t = b.getElementsByClassName("thumbnail");
+    const d = b.getElementsByClassName("description");
+    const r = (()=>{
+        let a=[], i=0;
+        while(i<t.length){
+            a.push({t:t[i],d:d[i]});
+            i++;
+        }
+        return a;
+    })();
+    let doc="";
+    r.forEach(v=>{
+        const url = v.d.childNodes[0].childNodes[0].attributes[0].nodeValue;
+        doc += url.replace("/atsumaru/games/","")+"\n";
+    });
+    b.innerText = doc;
+})()
