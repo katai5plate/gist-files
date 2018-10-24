246fff227f8797e2c4b8a3bb7e6279f043698407
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2018-10-24 11:11:17 +0900

    Create run-evcmd-json.js

---
+++ js/rpgmv/run-evcmd-json.js
@@ -0,0 +1,12 @@
+var callEvents = events => {
+  let inter = new Game_Interpreter();
+  inter._list = events;
+  $gameMap._commonEvents.push(inter);
+}
+
+callEvents([
+  { code: 101, parameters: ["Actor1", 0, 0, 2] },
+  { code: 401, parameters: ["あぁ～！水素の音ォ～！！"] },
+  { code: 101, parameters: ["Actor2", 0, 0, 2] },
+  { code: 401, parameters: ["あぁ～ぁ～！水素の音ォ～！！"] },
+])
