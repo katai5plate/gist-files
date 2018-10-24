750df1c7458ed9a71e4d834d878d2f3f792eb694
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2018-10-24 12:34:29 +0900

    Create game-ai-room-walker.js

---
+++ js/game-ai-room-walker.js
@@ -0,0 +1,23 @@
+(()=>{
+    // 記憶数とマップの繋がり
+    const historyLimit = 5, nodes = [
+        [],     [4],          [],
+        [4],    [1, 3, 5, 7], [4, 8],
+        [7, 9], [6, 10],      [5],
+        [6],    [7],          [],
+    ];
+    let index = 1, history = [];
+    // 彷徨い開始
+    for (let i = 0; i < 20; i++) {
+        const pindex = index;
+        while (history.includes(index)) {
+            if (nodes[index].length === 1) {
+                index = nodes[pindex][0];
+                break;
+            }
+            index = nodes[pindex][Math.random() * nodes[pindex].length >> 0];
+        }
+        history = history.concat(index).slice(-historyLimit);
+        console.log(index, history);
+    }
+})()
