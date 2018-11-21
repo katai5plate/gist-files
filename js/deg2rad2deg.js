7b503b54004d6b70bbf486c44b46d7bd11923462
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2018-11-21 09:53:06 +0900

    Update deg2rad2deg.js

--- js/deg2rad2deg.js
+++ js/deg2rad2deg.js
@@ -1,4 +1,6 @@
 const toRad = (Math.PI * 2) / 360;
 const toDeg = 360 / (Math.PI * 2);
+const limit = deg => (deg % 360 + 360) % 360;
 
 // const degree = 45, radian = deg * toRad;
+// limit(-90) // <- 270
