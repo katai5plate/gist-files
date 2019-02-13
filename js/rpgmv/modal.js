431319f1a940a76a308f67b208cfc4bd000627d5
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2019-02-13 16:44:39 +0900

    Update modal.js

--- js/rpgmv/modal.js
+++ js/rpgmv/modal.js
@@ -1,32 +1,49 @@
-var createDOM = ({name = "div", option = {}, style = {}} = {}) => {
-  const dom = Object.keys(option)
-    .reduce((p,c) => {p[c] = option[c]; return p}, document.createElement(name));
-  return Object.keys(style)
+var createDOM = ({name = "div", props = {}, style = {}, children = []} = {}) => {
+  let dom = Object.keys(props)
+    .reduce((p,c) => {p[c] = props[c]; return p}, document.createElement(name));
+  dom = Object.keys(style)
     .reduce((p,c) => {p.style[c] = style[c]; return p;}, dom);
+  children.forEach(child => dom.appendChild(child));
+  return dom;
 }
 
-var addModal = (id) => document.body.appendChild(
-  createDOM(
-    {
+var addModal = (id, innerHTML = "") => {
+  if(findModal(id).length !== 0) console.error("Already exists!");
+  document.body.appendChild(
+    createDOM({
       name: "div",
-      option: {id, className: "__modal"},
+      props: {
+        id,
+        className: "__modal",
+        innerHTML,
+      },
       style: {
         zIndex: 10000,
         position: "absolute",
         backgroundColor: "white",
         width: "320px",
         height: "240px",
-      }
-    }
+      },
+      children: [
+        createDOM({
+          name: "button",
+          props: {innerText: "CLOSE", onclick: () => removeModal(id)},
+          style: {position: "absolute", bottom: 0, fontSize: "24px"}
+        }),
+      ],
+    })
   )
-);
+};
 
-var removeModal = (id) => {
-  [...document.body.children]
+var findModal = (id) => {
+  return [...document.body.children]
     .filter(
       v =>
         v.id === id &&
         [...v.classList].includes("__modal")
-    )
-    .forEach(v => v.remove());
+    );
+}
+
+var removeModal = (id) => {
+  findModal(id).forEach(v => v.remove());
 }
