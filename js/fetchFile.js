20210dcf49a70e4f6e3995265f1a7c320e3d5f28
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2019-03-19 14:15:05 +0900

    Update fetchFile.js

--- js/fetchFile.js
+++ js/fetchFile.js
@@ -1,1 +1,4 @@
 var fetchText = async url => await (await fetch(url)).text();
+var fetchJson = async url => await (await fetch(url)).json();
+var fetchBuffer = async url => await (await fetch(url)).arrayBuffer();
+var fetchBlob = async url => await (await fetch(url)).blob();
