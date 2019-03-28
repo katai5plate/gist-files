20cdca57153f763c678f17c1f913c722bc93fdd6
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2019-03-28 14:37:12 +0900

    Update lerpPosition.js

--- js/lerpPosition.js
+++ js/lerpPosition.js
@@ -1,2 +1,2 @@
 var lerp = (x, y, a) => x + (y - x) * a;
-var lerp2D = ({x: x1, y: y1}, {x: x2, y: y2}, a) => ({x: lerp(x1, x2, a), y: lerp(y1, y2, a)});
+var lerp2D = ({x: x1, y: y1}, {x: x2, y: y2}, a) => ({x: x1 + (x2 - x1) * a, y: y1 + (y2 - y1) * a});
