9662d3dce11e65b5b352540171839fcbd06e3816
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2019-06-05 09:48:19 +0900

    Update postFetch.js

--- js/postFetch.js
+++ js/postFetch.js
@@ -1,4 +1,4 @@
-await(async(u,b,j="application/json")=>await
-fetch(u,{method:"POST",headers:{Accept:j,"Content-Type":j},
-body:JSON.stringify(b)})).json()
+await(async(u,b,j="application/json")=>(await fetch(
+u,{method:"POST",headers:{Accept:j,"Content-Type":j},
+body:JSON.stringify(b)})).json())
 ("http://localhost:3332/chat/post", {x:1});
