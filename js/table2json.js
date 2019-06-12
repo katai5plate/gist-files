84acb2b7fa6628f86cd4950d437cf9e5119416be
はどはど / Hadhad / Had2Apps <katai5plate.sub@gmail.com>
2019-06-12 19:23:44 +0900

    Update table2json.js

--- js/table2json.js
+++ js/table2json.js
@@ -1,4 +1,4 @@
-const table2Json = (query = "tr") => [...document.body.querySelectorAll(query)]
+const table2json = (query = "tr") => [...document.body.querySelectorAll(query)]
   .map(({children})=>[...[...children].map(({innerText})=>innerText)])
   .map((row,_,self)=>self[0].reduce((p,c,i)=>({...p, [c]:row.slice("-4")[i]}),{}))
   .slice(1)
