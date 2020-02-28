7aacf47aa53b79af50bd1f82d3a66d1a484247d9
Hadhad / HanoHano <katai5plate.sub@gmail.com>
2020-02-29 07:59:23 +0900

    Update H2A_ES9Checker.js

--- js/rpgmv/plugin/H2A_ES9Checker.js
+++ js/rpgmv/plugin/H2A_ES9Checker.js
@@ -33,39 +33,46 @@
  */
 
 (function() {
-  try {
-    eval(
-      "(async()=>{await new Promise(r=>Promise.resolve(0).finally(()=>r()))})()"
-    );
-    eval(
-      "(async()=>{const arr=new Array(5).map(v=>Promise.resolve(v));for await(let a of arr){}})()"
-    );
-    eval(
-      "(()=>{var {a,...r}={a:1,b:2,c:3};var x={...r,b:1,c:[...[0,1,2]]}})()"
-    );
-    eval("(()=>{if(!/x.y/s.test('x\\ny'))throw new Error()})()");
-    eval(
-      '(()=>{if(JSON.stringify(/(?<a>\\d{1})-(?<b>\\d{2})/u.exec(\'1-22\').groups)!==\'{"a":"1","b":"22"}\')throw new Error()})()'
-    );
-    eval(
-      "(()=>{if('1-22'.replace(/(?<a>\\d{1})-(?<b>\\d{2})/u,'$<a>/$<b>')!=='1/22')throw new Error()})()"
-    );
-    eval("(()=>{if(/^(?<x>.*).k<x>$/u.test('a*a'))throw new Error()})()");
-    eval(
-      "(()=>{['123456'.match(/123(?=456)/),'123'.match(/123(?!456)/),'123456'.match(/(?<=123)456/),'456'.match(/(?<!123)456/)].includes(null)})()"
-    );
-    eval("(()=>{if(!/^\\p{sc=Hiragana}/u.test('あ'))throw new Error()})()");
-  } catch (error) {
-    SceneManager.stop();
-    Graphics.printError(
-      "お使いのブラウザはサポートされておりません。<br />以下のブラウザでお試しください。",
-      [
-        "Chrome 80 以上 / Edge 80 以上",
-        "Opera 66 以上 / iOS Safari 13.2 以上",
-        "Android Browser 80 以上",
-        "Samsung Internet 10.1 以上"
-      ].join("<br />")
-    );
-    AudioManager.stopAll();
+  function check() {
+    try {
+      eval(
+        "(async()=>{await new Promise(r=>Promise.resolve(0).finally(()=>r()))})()"
+      );
+      eval(
+        "(async()=>{const arr=new Array(5).map(v=>Promise.resolve(v));for await(let a of arr){}})()"
+      );
+      eval(
+        "(()=>{var {a,...r}={a:1,b:2,c:3};var x={...r,b:1,c:[...[0,1,2]]}})()"
+      );
+      eval("(()=>{if(!/x.y/s.test('x\\ny'))throw new Error()})()");
+      eval(
+        '(()=>{if(JSON.stringify(/(?<a>\\d{1})-(?<b>\\d{2})/u.exec(\'1-22\').groups)!==\'{"a":"1","b":"22"}\')throw new Error()})()'
+      );
+      eval(
+        "(()=>{if('1-22'.replace(/(?<a>\\d{1})-(?<b>\\d{2})/u,'$<a>/$<b>')!=='1/22')throw new Error()})()"
+      );
+      eval("(()=>{if(/^(?<x>.*).k<x>$/u.test('a*a'))throw new Error()})()");
+      eval(
+        "(()=>{['123456'.match(/123(?=456)/),'123'.match(/123(?!456)/),'123456'.match(/(?<=123)456/),'456'.match(/(?<!123)456/)].includes(null)})()"
+      );
+      eval("(()=>{if(!/^\\p{sc=Hiragana}/u.test('あ'))throw new Error()})()");
+    } catch (error) {
+      SceneManager.stop();
+      Graphics.printError(
+        "お使いのブラウザはサポートされておりません。<br />以下のブラウザでお試しください。",
+        [
+          "Chrome 80 以上 / Edge 80 以上",
+          "Opera 66 以上 / iOS Safari 13.2 以上",
+          "Android Browser 80 以上",
+          "Samsung Internet 10.1 以上"
+        ].join("<br />")
+      );
+      AudioManager.stopAll();
+    }
   }
+  var i = setInterval(() => {
+    if (window.Graphics && window.SceneManager && window.AudioManager)
+      clearInterval(i);
+    check();
+  }, 1);
 })();
