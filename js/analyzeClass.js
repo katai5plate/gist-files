74845b37b720e192638d5cfede0f000ab4a4fc37
Hadhad / HanoHano <katai5plate.sub@gmail.com>
2021-09-13 07:30:40 +0900

    Update analyzeClass.js

--- js/analyzeClass.js
+++ js/analyzeClass.js
@@ -1,7 +1,20 @@
-var analyzeClass = (C) => {
+/**
+ * クラスを分析する。継承されたメンバ変数までは調べない。
+ * @param {ClassDecorator} C 分析するクラス
+ * @param {boolean} noCode ソースコードを出力しない
+ * @returns {{functions,members,prototypes}} 検出した関数・メンバ変数・継承クラス名
+ */
+window.analyzeClass = (C, noCode = false) => {
   const entries = Object.entries(C.prototype || C);
-  // 関数リスト
-  const functions = (
+  // 関数リストを作成。まずは現在のクラスで定義されたものを取得。
+  const toFunctionData = ([name, fn]) => ({
+    name,
+    isReadable: !fn?.toString()?.match(/\[native code\]/), // Proxy の場合こうなる
+    code: fn?.toString() || "",
+    extendedBy: C.name || C.constructor.name,
+    isMember: false,
+  });
+  let functions = (
     entries.length
       ? // ES5 class
         entries
@@ -10,13 +23,86 @@ var analyzeClass = (C) => {
           (p) => [p, C.prototype[p]],
           []
         )
-  ).map(([name, v]) => ({
-    name,
-    code: v?.toString() || "",
-  }));
-  // メンバ変数リスト
-  const members = Object.entries(
+  ).map(toFunctionData);
+  /**
+   * 代入値から型を推測する関数
+   * @param {string[]} fnNames 検索先の関数名
+   * @param {string} name 検索するメンバ変数名
+   * @returns {{type:string,values:string[]}}
+   */
+  const getPredictedType = (fnNames, name) => {
+    const IS_FUNC = "@@FUNC@@";
+    /**
+     * 関数のソースコードから代入値か関数かどうか（IS_FUNC）を取得
+     * @param {string} code 関数のソースコード
+     * @returns {string}
+     */
+    const codeToValue = (code) =>
+      code?.match(new RegExp(`this\.${name} *?\\(`))
+        ? IS_FUNC
+        : code?.match(
+            new RegExp(`this\.${name} *?= *?([_"'\+\-\.\`0-9a-zA-Z]+) *?;`)
+          )?.[1];
+    // 代入された値リスト
+    const values = [
+      ...new Set(
+        fnNames
+          .map((n) =>
+            codeToValue(
+              functions.filter((x) => x.isReadable).find((f) => f.name === n)
+                .code
+            )
+          )
+          .filter(Boolean)
+      ),
+    ];
+    // 判定条件
+    const isBoolean = (x) => ["true", "false"].includes(x);
+    const isNumber = (x) => !Number.isNaN(Number(x)) || Number.isNaN(x);
+    const isString = (x) =>
+      x.match(/^".*?"/) || x.match(/^'.*?'/ || x.match(/^`.*?`/));
+    const isVoid = (x) => ["null", "undefined"].includes(x);
+    // 判定処理
+    if (values.length === 0) return { type: "unknown" };
+    if (values.find((x) => x === IS_FUNC)) return { type: "function" };
+    if (values.every(isBoolean)) return { type: "boolean", values };
+    if (values.every(isNumber)) return { type: "number", values };
+    if (values.every(isString)) return { type: "string", values };
+    if (values.every(isVoid)) return { type: "void", values };
+    // 複合的な型だった場合、あり得る型を列挙する
+    if (
+      values.every((x) =>
+        [isBoolean, isNumber, isString, isVoid].find((y) => y(x))
+      )
+    )
+      return {
+        type: `${[
+          ...new Set(
+            values.reduce(
+              (p, c) => [
+                ...p,
+                isBoolean(c)
+                  ? "boolean"
+                  : isNumber(c)
+                  ? "number"
+                  : isString(c)
+                  ? "string"
+                  : isVoid(c)
+                  ? "void"
+                  : c,
+              ],
+              []
+            )
+          ),
+        ].join(" | ")} | unknown`,
+        values,
+      };
+    return { type: "unknown", values };
+  };
+  // メンバ変数リストを作成
+  let members = Object.entries(
     functions
+      .filter((x) => x.isReadable)
       .reduce(
         (p, { name: fnName, code }) => [
           ...p,
@@ -36,13 +122,44 @@ var analyzeClass = (C) => {
         }),
         {}
       )
-  ).map(([name, v]) => ({ name, fnNames: [...new Set(v)] }));
+  )
+    .map(([name, v]) => ({ name, fnNames: [...new Set(v)] }))
+    .map(({ name, fnNames }) => ({
+      name,
+      fnNames,
+      predictedType: getPredictedType(fnNames, name),
+    }));
   // 継承リスト
   let { prototype } = C;
   let prototypes = [];
   while (prototype) {
-    prototypes = [...prototypes, prototype?.constructor?.name || ""];
+    prototypes = [...prototypes, prototype || ""];
     prototype = Object.getPrototypeOf(prototype);
   }
-  return { functions, members, prototypes };
+  // メンバ変数で検出した関数を継承された関数として functions に移動
+  functions = [
+    ...functions,
+    ...members
+      .filter((x) => x.predictedType.type === "function")
+      .map((x) => {
+        const extended = prototypes
+          .slice(1)
+          .reverse()
+          .find((p) => p[x.name]);
+        return {
+          ...toFunctionData([x.name, extended[x.name]]),
+          extendedBy: extended.constructor.name,
+          isMember: true,
+        };
+      }),
+  ];
+  // メンバ変数リストから関数を削除
+  members = members.filter((x) => x.predictedType.type !== "function");
+  if (noCode) functions = functions.map(({ code, ...rest }) => rest);
+  // 解析結果を出力
+  return {
+    functions,
+    members,
+    prototypes: prototypes.map((x) => x.constructor.name),
+  };
 };
